1007049d462d64867c200d5212a351ae3a93688a
sarbjit-rnd <sarbjit.rndexperts@gmail.com>
2022-12-21 12:55:58 +0530

    first commit

---
+++ readme.txt
@@ -0,0 +1,60 @@
+=== Sip Calculator ===
+Contributors: RND Experts, Rajeev Kumar
+Donate link: https://paypal.me/rndexperts
+Tags: sip, investment
+Requires at least: 4.5
+Tested up to: 6.1.1
+Requires PHP: 5.6
+Stable tag: 0.1.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin can be used to calculate Systematic Investment Plan (SIP).
+
+== Description ==
+
+This plugin can be used to calculate Systematic Investment Plan (SIP). You can place shortcode `[sip-calculator]` anywhere on your templates, pages, posts.
+
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `sip-calculator.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place `<?php echo do_shortcode( '[sip-calculator]' ); ?>` in your templates,page or post
+
+== Frequently Asked Questions ==
+
+= Is It works with Block editor =
+
+Yes, You can use shortcode to use this form.
+
+= What about foo bar? =
+
+Answer to foo bar dilemma.
+
+== Screenshots ==
+
+1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
+the /assets directory or the directory that contains the stable readme.txt (tags or trunk). Screenshots in the /assets
+directory take precedence. For example, `/assets/screenshot-1.png` would win over `/tags/4.3/screenshot-1.png`
+(or jpg, jpeg, gif).
+2. This is the second screen shot
+
+== Changelog ==
+
+= 0.1.0 =
+* Bootstrap assets.
+* Another change.
+
+
+
+== A brief Markdown Example ==
+
+Ordered list:
+
+1. Block Editor support.
+2. Sip Form with Pie chart 
---
+++ sip-calculator.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Plugin Name:     Sip Calculator
+ * Plugin URI:      rndexperts.com
+ * Description:     This plugin can be used to calculate Systematic Investment Plan (SIP). You can place shortcode **[sip-calculator]** anywhere on your templates, pages or posts.
+ * Author:          Rnd Experts
+ * Author URI:      rndexperts.com
+ * Text Domain:     sip-calculator
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Sip_Calculator
+ */
+$url = plugin_dir_url(__FILE__);
+$path =  plugin_dir_path(__FILE__);
+define('Sip_Calculator_Url',$url);
+define('Sip_Calculator_Path',$path);
+/**
+ * Registers a stylesheet.
+ */
+function rnd_register_plugin_styles() {
+	wp_register_style( 'sip-bootstrapcdn', '//maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css' );
+	wp_register_script( 'sip-bootstrapcdnjs', '//maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js' , array('jquery'), '', false );
+	wp_register_script( 'sip-chartsjs', '//www.gstatic.com/charts/loader.js' , array('jquery'), '', false );
+	
+}
+// Register style sheet.
+add_action( 'wp_enqueue_scripts', 'rnd_register_plugin_styles' );
+
+function rnd_loadPluginTextdomain()
+{
+	load_plugin_textdomain('sip-calculator', false, basename(dirname(__FILE__)) . '/languages/');
+}
+add_action('init', 'rnd_loadPluginTextdomain');
+
+ function rnd_sip_calc_func( $atts ) {
+	$a = shortcode_atts( array(
+		'invested_amount_monthly' => 2000,
+		'estimated_return_rate' => 10,
+		'years' => 5,
+	), $atts );
+	wp_enqueue_style( 'sip-bootstrapcdn' );
+	wp_enqueue_script('sip-bootstrapcdnjs');
+	wp_enqueue_script('sip-chartsjs');
+    ob_start();
+    include  Sip_Calculator_Path . 'includes/index.php';
+    return ob_get_clean();
+
+	
+}
+add_shortcode( 'sip-calculator', 'rnd_sip_calc_func' );
