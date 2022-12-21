41ee0e6f2e825eda9d009ec10b53967c7870bb87
webrndexperts <webrndexperts@gmail.com>
2022-12-21 15:48:27 +0530

    logic and index file update

--- includes/index.php
+++ includes/index.php
@@ -6,25 +6,31 @@
             <h2> <?php echo __('SIP calculator', ' sip-calculator'); ?> </h2>
             <div class="card-body">
                 <form action="" method="post">
-                <div class="form-group">
-                    <p id="feedback"><p>
+                    <div class="form-group">
+                        <p id="feedback"></p>
 
                     </div>
                     <div class="form-group">
-                    <label for="investment"> <?php echo __('Monthly investment amount', ' sip-calculator'); ?>:</label>
-                        <input type="number" id="investment_amount" name="investment" pattern="[0-9]+"  title="<?php echo __('please enter number only', ' sip-calculator'); ?>" value="<?php echo esc_attr($a['invested_amount_monthly']); ?>"
-                            class="form-control">
+                        <label for="investment">
+                            <?php echo __('Monthly investment amount', ' sip-calculator'); ?>:</label>
+                        <input type="number" id="investment_amount" name="investment" pattern="[0-9]+"
+                            title="<?php echo __('please enter number only', ' sip-calculator'); ?>"
+                            value="<?php echo esc_attr($a['invested_amount_monthly']); ?>" class="form-control">
 
                     </div>
                     <div class="form-group">
-                    <label for="return"> <?php echo __('Expected return rate (in percentage)', ' sip-calculator'); ?>:</label>
-                        <input type="number" id="return_rate" name="return rate" pattern="[0-9]+" title="<?php echo __('please enter number only', ' sip-calculator'); ?>" value="<?php echo esc_attr($a['estimated_return_rate']); ?>"
-                            class="form-control">
+                        <label for="return">
+                            <?php echo __('Expected return rate (in percentage)', ' sip-calculator'); ?>:</label>
+                        <input type="number" id="return_rate" name="return rate" pattern="[0-9]+"
+                            title="<?php echo __('please enter number only', ' sip-calculator'); ?>"
+                            value="<?php echo esc_attr($a['estimated_return_rate']); ?>" class="form-control">
 
                     </div>
                     <div class="form-group">
-                    <label for="year"> <?php echo __('Time period (in years)', ' sip-calculator'); ?>:</label>
-                        <input type="number" id="year" name="year" pattern="[0-9]+" title="<?php echo __('please enter number only', ' sip-calculator'); ?>" value="<?php echo esc_attr($a['years']); ?>"  class="form-control">
+                        <label for="year"> <?php echo __('Time period (in years)', ' sip-calculator'); ?>:</label>
+                        <input type="number" id="year" name="year" pattern="[0-9]+"
+                            title="<?php echo __('please enter number only', ' sip-calculator'); ?>"
+                            value="<?php echo esc_attr($a['years']); ?>" class="form-control">
 
                     </div>
                     <div class="">
@@ -42,11 +48,11 @@
                         <th><span id="print_invested_amount"> 0 </span></th>
                     </tr>
                     <tr>
-                        <th> <?php echo __('Estimated Gain amount', ' sip-calculator'); ?>  </th>
+                        <th> <?php echo __('Estimated Gain amount', ' sip-calculator'); ?> </th>
                         <th><span id="print_est_return"> 0 </span></th>
                     </tr>
                     <tr>
-                        <th> <?php echo __('Total amount', ' sip-calculator'); ?>  </th>
+                        <th> <?php echo __('Total amount', ' sip-calculator'); ?> </th>
                         <th><span id="print_total_value"> 0 </span></th>
                     </tr>
                 </table>
@@ -69,69 +75,79 @@ btn.addEventListener('click', () => {
     var investment_amount = document.getElementById('investment_amount').value;
     var return_rate = document.getElementById('return_rate').value;
     var year = document.getElementById('year').value;
-    document.getElementById('feedback').innerHTML="";
+    document.getElementById('feedback').innerHTML = "";
     document.getElementById("feedback").style.color = "white";
-    if(investment_amount && return_rate && year){
-    jQuery.ajax({
-        url: '<?php echo Sip_Calculator_Url . "includes/logic.php"; ?>',
-        method: "post",
-        data: {
-            investment: investment_amount,
-            return_rate: return_rate,
-            year: year
-        },
-        success: function(data) {
-            data = JSON.parse(data);
-            print_invested_amount.innerHTML = parseInt(data.invested_amount);
-            print_est_return.innerHTML = parseInt(data.est_return);
-            print_total_value.innerHTML = parseInt(data.total_value);
-            /////////   starts  coding  chart /////
-            var est_return = parseInt(data.est_return);
-            var invested_amount = parseInt(data.invested_amount);
-			var total_value = parseInt(data.total_value);
-            google.charts.load("current", {
-                packages: ["corechart"]
-            });
-            google.charts.setOnLoadCallback(drawChart);
-            function drawChart() {
-                var data = google.visualization.arrayToDataTable([
-					['<?php echo __('Task', ' sip-calculator'); ?>', '<?php echo __('invested amount', ' sip-calculator'); ?>'],
-                    ['<?php echo __('Invested amount', ' sip-calculator'); ?>', invested_amount],
-                    ['<?php echo __('Estimated Gain amount', ' sip-calculator'); ?>', est_return],
-					['<?php echo __('Total amount', ' sip-calculator'); ?>', total_value],
-                ]);
+    if (investment_amount && return_rate && year) {
+        jQuery.ajax({
+            url: '<?php echo Sip_Calculator_Url . "includes/logic.php"; ?>',
+            method: "post",
+            data: {
+                investment: investment_amount,
+                return_rate: return_rate,
+                year: year
+            },
+            success: function(data) {
+                data = JSON.parse(data);
+                print_invested_amount.innerHTML = parseInt(data.invested_amount);
+                print_est_return.innerHTML = parseInt(data.est_return);
+                print_total_value.innerHTML = parseInt(data.total_value);
+                /////////   starts  coding  chart /////
+                var est_return = parseInt(data.est_return);
+                var invested_amount = parseInt(data.invested_amount);
+                var total_value = parseInt(data.total_value);
+                google.charts.load("current", {
+                    packages: ["corechart"]
+                });
+                google.charts.setOnLoadCallback(drawChart);
 
-                var options = {
-                    title: '<?php echo __('SIP Pie Chart', ' sip-calculator'); ?>',
-                    pieHole: 0.4,
-                    slices: {
-                        0: {
-                            color: '#00d09c'
-                        },
-                        1: {
-                            color: '#5367ff'
-                        },
-						2: {
-                            color: '#146c43'
+                function drawChart() {
+                    var data = google.visualization.arrayToDataTable([
+                        ['<?php echo __('Task', ' sip-calculator'); ?>',
+                            '<?php echo __('invested amount', ' sip-calculator'); ?>'
+                        ],
+                        ['<?php echo __('Invested amount', ' sip-calculator'); ?>',
+                            invested_amount
+                        ],
+                        ['<?php echo __('Estimated Gain amount', ' sip-calculator'); ?>',
+                            est_return
+                        ],
+                        ['<?php echo __('Total amount', ' sip-calculator'); ?>',
+                            total_value
+                        ],
+                    ]);
+
+                    var options = {
+                        title: '<?php echo __('SIP Pie Chart', ' sip-calculator'); ?>',
+                        pieHole: 0.4,
+                        slices: {
+                            0: {
+                                color: '#00d09c'
+                            },
+                            1: {
+                                color: '#5367ff'
+                            },
+                            2: {
+                                color: '#146c43'
+                            }
+
                         }
-
                     }
+
+                    var chart = new google.visualization.PieChart(document.getElementById(
+                        'piechart'));
+                    chart.draw(data, options);
                 }
 
-                var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-                chart.draw(data, options);
+                /////    end coding of chart////
+
             }
 
-            /////    end coding of chart////
+        })
+    } else {
 
-        }
-
-    })
-}else{
-    
-    document.getElementById('feedback').innerHTML="* All fields are required.";
-    document.getElementById("feedback").style.color = "red";
-}
+        document.getElementById('feedback').innerHTML = "* All fields are required.";
+        document.getElementById("feedback").style.color = "red";
+    }
 
 });
 </script>
--- includes/logic.php
+++ includes/logic.php
@@ -1,20 +1,19 @@
 <?php
-if(isset($_POST['investment']) && isset($_POST['return_rate']) && isset($_POST['year'])){
-$investment= intval($_POST['investment']);
-$annualRate= intval($_POST['return_rate']);
-$years= intval($_POST['year']);
-$monthlyRate=$annualRate/12/100;
-$months=$years*12;
-$futureValue=0;
-$futureValue=$investment*((pow(1+$monthlyRate,$months)-1)/$monthlyRate)*(1+$monthlyRate);
-$investment_amount=$investment*12*$years;
-$est_rtn=$futureValue-$investment_amount;
- $data=array(
-'invested_amount'=>intval($investment_amount),
-'est_return'=>intval($est_rtn),
-'total_value'=>intval($futureValue)
-);
-echo json_encode($data);
+if (isset($_POST['investment']) && isset($_POST['return_rate']) && isset($_POST['year'])) {
+    $investment = intval($_POST['investment']);
+    $annualRate = intval($_POST['return_rate']);
+    $years = intval($_POST['year']);
+    $monthlyRate = $annualRate / 12 / 100;
+    $months = $years * 12;
+    $futureValue = 0;
+    $futureValue = $investment * ((pow(1 + $monthlyRate, $months) - 1) / $monthlyRate) * (1 + $monthlyRate);
+    $investment_amount = $investment * 12 * $years;
+    $est_rtn = $futureValue - $investment_amount;
+    $data = array(
+        'invested_amount' => intval($investment_amount),
+        'est_return' => intval($est_rtn),
+        'total_value' => intval($futureValue),
+    );
+    echo json_encode($data);
 }
 die();
-?>
